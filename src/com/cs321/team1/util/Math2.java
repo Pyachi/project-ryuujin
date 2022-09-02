13572ec887cbe367d1e1339e2253c3fb36f6d5b1
Pyachi <snglass2002@hotmail.com>
2022-09-02 14:08:29 -0500

    Removed unnecessary utilities

--- src/com/cs321/team1/util/Math2.java
+++
@@ -1,7 +0,0 @@
-package com.cs321.team1.util;
-
-public class Math2 {
-    public static int clamp(int val, int min, int max) {
-        return java.lang.Math.max(min, java.lang.Math.min(max, val));
-    }
-}
