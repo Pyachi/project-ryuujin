ad7184eb16ca2635c1690ad79bfc2d5b39531304
Sarah Glass <snglass2002@hotmail.com>
2022-09-27 13:18:53 -0500

    Added music, needs more adjustments

--- src/com/cs321/team1/framework/menu/menus/MainMenu.java
+++ src/com/cs321/team1/framework/menu/menus/MainMenu.java
@@ -3,6 +3,7 @@ package com.cs321.team1.framework.menu.menus;
 import com.cs321.team1.framework.Game;
 import com.cs321.team1.framework.menu.Menu;
 import com.cs321.team1.framework.menu.elements.MenuButton;
+import com.cs321.team1.framework.sounds.Music;
 
 public class MainMenu extends Menu {
     public MainMenu() {
@@ -10,6 +11,7 @@ public class MainMenu extends Menu {
         elements.add(new MenuButton("Continue", () -> {}));
         elements.add(new MenuButton("Options", () -> Game.get().pushSegment(new OptionsMenu(null))));
         elements.add(new MenuButton("Save and Quit", () -> System.exit(0)));
+        Music.DAY.play();
     }
     
     @Override
--- src/com/cs321/team1/framework/sounds/Music.java
+++ src/com/cs321/team1/framework/sounds/Music.java
@@ -1,7 +1,34 @@
 package com.cs321.team1.framework.sounds;
 
-public class Music {
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+//TODO Allow changing of music & dynamic volume adjustment
+public enum Music {
+    DAY("src/resources/music/day.wav");
+    
+    private final File path;
     private static int volume = 50;
+    
+    Music(String path) {
+        this.path = new File(path).getAbsoluteFile();
+    }
+    
+    public void play() {
+        try {
+            var clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(path));
+            ((FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN)).setValue((float) (20 *
+                    Math.log10(volume / 100.0)));
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ignored) {
+        }
+    }
     
     public static int getVolume() {
         return volume;
