76c24ac15c3de61b6d3ebeb29767d09548622240
Sarah Glass <snglass2002@hotmail.com>
2022-10-04 06:04:58 -0500

    Removed multiplayer debug world

--- src/com/cs321/team1/framework/map/Worlds.java
+++ src/com/cs321/team1/framework/map/Worlds.java
@@ -10,13 +10,10 @@ import com.cs321.team1.framework.textures.Textures;
 public class Worlds {
     public static Level worldOne() {
         var world = Level.emptyLevel(9, 5);
-        world.addObject(new UnpassableTile(Location.atTile(3,3), Textures.BOULDER.get()));
         world.addObject(new Trigger(Location.atTile(5, 3), Textures.CRATE.get(), () -> {
             if (Controls.SELECT.isPressed()) Game.get().pushSegment(Level.loadLevel("one"));
         }));
-        world.addObject(new Player(Location.atTile(1, 2)));
-        world.addObject(new Player(Location.atTile(1, 3)));
-        world.addObject(new Player(Location.atTile(1, 4)));
+        world.addObject(new Player(Location.atTile(5, 1)));
         return world;
     }
 }
